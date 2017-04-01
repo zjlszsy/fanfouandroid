588323255c4f1ac027c474c8d0bffe1e276191a2
wentingz <zhouwentingzj@gmail.com>
2017-03-31 22:59:49 -0700

    Usertimeline activity

--- app/src/main/java/com/wentingzhou/android/fanfouclient/UserTimelineActivity.java
+++ app/src/main/java/com/wentingzhou/android/fanfouclient/UserTimelineActivity.java
@@ -5,27 +5,34 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
 import com.wentingzhou.android.fanfouclient.model.FanfouStatus;
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Created by wendyzhou on 3/31/2017.
+ */
 
-public class DisplayTimelineActivity extends Activity {
+public class UserTimelineActivity extends Activity {
+    public static final String USERTIMELINEURL = "URL";
     public static final String USERNAME = "username";
     public static final String PASSWORD = "password";
-    public static final String FAKEURL = "http://api.fanfou.com/statuses/friends_timeline.xml";
 
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle currentBundle) {
+        super.onCreate(currentBundle);
         setContentView(R.layout.main);
         HttpRequest request = new HttpRequest();
+        String url = getIntent().getStringExtra(USERTIMELINEURL);
+        request.mURL = url;
+
         String userName = getIntent().getStringExtra(USERNAME);
         request.mUsernameInput = userName;
 
         String passWord = getIntent().getStringExtra(PASSWORD);
         request.mPasswordInput = passWord;
-        List<FanfouStatus> statusList = null;
+
+        List<FanfouStatus> statusList = new ArrayList<FanfouStatus>();
         try {
-            statusList = request.execute(FAKEURL).get();
+            statusList = request.execute(url).get();
         } catch (Exception e){
             Log.e("Exception", "detail", e);
         }
@@ -35,8 +42,4 @@ public class DisplayTimelineActivity extends Activity {
         ListView feedList = (ListView) findViewById(R.id.list);
         feedList.setAdapter(adaptor);
     }
-
 }
-
-
-
